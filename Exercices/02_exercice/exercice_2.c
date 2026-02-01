64c4476b43f3df8b0aaea88a93d9af9bdfb96cba
unknown <Teva-Clairefond@users.noreply.github.com>
2026-02-01 16:21:24 +0100

    Mise à jour de l'exercice 2

--- Exercices/02_exercice/exercice_2.c
+++ Exercices/02_exercice/exercice_2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main () {
-    printf ("Je m'appelle Teva\n Je suis né le 21 août 2001\n J'habite à Auriol");
+    printf ("Je m'appelle Thomas\n Je suis né le 20 Juillet 2000\n J'habite à Paris");
     return 0;
 }
